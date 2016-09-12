bb3e312548df3deae53e082b265d10e19fa0fc99
김보운 <haioer@naver.com>
2016-09-12 14:56:27 +0900

    fragment change youtubeplayer

--- app/src/main/java/com/videovillage/application/video/VideoPlayActivity.java
+++ app/src/main/java/com/videovillage/application/video/VideoPlayActivity.java
@@ -1,11 +1,14 @@
 package com.videovillage.application.video;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
 import com.androidquery.AQuery;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 import com.videovillage.application.R;
+import com.videovillage.application.constant.Constant;
 import com.videovillage.application.data.DataManager;
 import com.videovillage.application.data.VideoEntry;
 import com.videovillage.application.thread.VideoSearchThread;
@@ -13,8 +16,12 @@ import com.videovillage.application.thread.VideoSearchThread;
 /**
  * Created by secret on 9/4/16.
  */
-public class VideoPlayActivity extends Activity {
-    AQuery aq = new AQuery(this);
+public class VideoPlayActivity extends YouTubeFailureRecoveryActivity implements YouTubePlayer.OnInitializedListener, YouTubePlayer.OnFullscreenListener {
+    private YouTubePlayer player;
+    private YouTubePlayerView playerView;
+    private String videoId;
+    private boolean isFullscreen;
+    private AQuery aq = new AQuery(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +31,59 @@ public class VideoPlayActivity extends Activity {
         Intent intent = getIntent();
         VideoEntry videoEntry = (VideoEntry) intent.getSerializableExtra("VideoEntry");
 
-        VideoFragment videoFragment =
-                (VideoFragment) getFragmentManager().findFragmentById(R.id.video_fragment_container);
-        videoFragment.setVideoId(videoEntry.getVideoId());
+        playerView = (YouTubePlayerView) findViewById(R.id.video_player_youtube_player);
+        playerView.initialize(Constant.YOUTUBE_ANDROID_API_KEY, this);
+        setVideoId(videoEntry.getVideoId());
 
         VideoSearchThread videoSearchThread = new VideoSearchThread(videoEntry.getVideoId());
 
         aq.id(R.id.video_description_text).text(DataManager.getDataManager().getVideoInfo().getDescription());
+    }
+
+    @Override
+    protected YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return playerView;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (player != null) {
+            player.release();
+        }
+        super.onDestroy();
+    }
+
+    public void setVideoId(String videoId) {
+        if (videoId != null && !videoId.equals(this.videoId)) {
+            this.videoId = videoId;
+            if (player != null) {
+                player.cueVideo(videoId);
+            }
+        }
+    }
+
+    public void pause() {
+        if (player != null) {
+            player.pause();
+        }
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player, boolean restored) {
+        this.player = player;
+        player.setOnFullscreenListener(this);
+        if (!restored && videoId != null) {
+            player.cueVideo(videoId);
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult result) {
+        this.player = null;
+    }
+
+    @Override
+    public void onFullscreen(boolean isFullscreen) {
+        this.isFullscreen = isFullscreen;
     }
 }
