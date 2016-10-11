df32ba7644fcf60c0e010b8b1752c1f6bb1d6016
김보운 <haioer@naver.com>
2016-10-11 13:41:16 +0900

    add Firebase & ListView ViewHolder Pattern

---
+++ app/src/main/java/com/videovillage/application/adapter/ViewHolder.java
@@ -0,0 +1,14 @@
+package com.videovillage.application.adapter;
+
+import android.widget.TextView;
+
+import com.google.android.youtube.player.YouTubeThumbnailView;
+
+/**
+ * Created by BoWoon on 2016-10-11.
+ */
+
+public class ViewHolder {
+    public TextView videoTitle;
+    public YouTubeThumbnailView thumbnail;
+}
